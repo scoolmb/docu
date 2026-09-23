3d00282cbf160c28020b3317486c69a49091daac
scoolmb <scoolmabrouki@gmail.com>
2026-09-23 13:15:50 +0000

    docs update footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,13 +1,13 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Dropsy',
-  tagline: 'The token distribution layer for Solana',
-  favicon: 'img/favicon.ico',
+  title: "Dropsy",
+  tagline: "The token distribution layer for Solana",
+  favicon: "img/favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -15,49 +15,49 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://dropsy.org/',
+  url: "https://dropsy.org/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'dropsy labs', // Usually your GitHub org/user name.
-  projectName: 'dropsy', // Usually your repo name.
+  organizationName: "dropsy labs", // Usually your GitHub org/user name.
+  projectName: "dropsy", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          path: 'docs',       // folder in your project
-          routeBasePath: '/', // 🚀 docs appear at site root
+          sidebarPath: "./sidebars.ts",
+          path: "docs", // folder in your project
+          routeBasePath: "/", // 🚀 docs appear at site root
           //versions: false,    // disable versioning
         },
 
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -68,22 +68,22 @@ const config: Config = {
       {
         hashed: true,
         indexPages: true,
-        docsRouteBasePath: '/'
-      }
+        docsRouteBasePath: "/",
+      },
     ],
   ],
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     colorMode: {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'Dropsy',
+      title: "Dropsy",
       logo: {
-        alt: 'Dropsy Logo',
-        src: 'img/logo.png',
+        alt: "Dropsy Logo",
+        src: "img/logo.png",
       },
 
       items: [
@@ -96,7 +96,7 @@ const config: Config = {
       ],
     },
     footer: {
-      style: 'light',
+      style: "light",
       links: [
         /*{
           title: 'Docs',
@@ -108,28 +108,28 @@ const config: Config = {
           ],
         },*/
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'X (Twitter) ',
-              to: 'https://x.com/dropsy_protocol',
+              label: "X (Twitter) ",
+              to: "https://x.com/dropsy_protocol",
             },
             {
-              label: 'Discord',
-              to: 'https://discordapp.com/invite/docusaurus',
+              label: "Discord",
+              to: "https://discord.gg/CwX9exeWY7",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Website',
-              to: 'https://www.dropsy.xyz/',
+              label: "Website",
+              to: "https://www.dropsy.org/",
             },
             {
-              label: 'App',
-              to: 'https://app.dropsy.xyz/',
+              label: "App",
+              to: "https://app.dropsy.org/",
             },
           ],
         },
